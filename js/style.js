52395ddc09233f9c932f79fe18a1b11e23221563
nameverb <drew.stanleymso@gmail.com>
2016-11-16 17:49:34 -0600

    attempted data scroll

--- js/style.js
+++ js/style.js
@@ -1,35 +1,16 @@
 $(document).ready(function(){
 
-/* activate sidebar */
-
-$('#sidebar').affix({
-  offset: {
-    top: 200,
-	bottom: 400
-  }
-});
-
-/* activate scrollspy menu */
-var $body   = $(document.body);
-var navHeight = $('.navbar').outerHeight(true) + 10;
-
-$body.scrollspy({
-	target: '#leftCol',
-	offset: navHeight
-});
-
-/* smooth scrolling sections */
-$('a[href*=#]:not([href=#])').click(function() {
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 50
+        $('html, body').animate({
+          scrollTop: target.offset().top
         }, 1000);
         return false;
       }
     }
-});
-
+  });
 });
